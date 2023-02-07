677727ac3272343ab69568922be4a5829f52581f
Julian Bellafronte <71682040+Julianbellafronte@users.noreply.github.com>
2023-02-07 09:45:57 -0300

    Delete Spotify to Youtube to Mp3 directory

--- Spotify to Youtube to Mp3/error.py
+++
@@ -1,5 +0,0 @@
-print("\a")
-print("=======================================")
-print("|Se genero un error.|")  
-print("=======================================")
-print("\n","Continuando con la descarga...") 
